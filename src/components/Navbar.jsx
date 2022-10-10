9a997b0669cc4db43332e32abe20af1c1af01ab4
shreyas0924 <shreyasp0924@gmail.com>
2022-10-10 22:21:53 +0530

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {close, logo, menu} from '../assets'
+import { navLinks } from "../constants";
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false)
+
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='flex w-full py-6 justify-between items-center'>
+      <img src={logo} alt='hooback' className='w-[120px] h-[32px] ' />
+      {/* Desktop */}
+      <ul className='list-none flex-1 justify-end items-center sm:flex hidden'>
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${index === navLinks.length-1 ? 'mr-0' : 'mr-10' } `} > 
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile*/}
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img 
+            src={toggle ? close : menu} 
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle((prev) => !prev )}
+          />
+
+          <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 rounded-xl sidebar min-w-[140px]` }> 
+            <ul className='list-none flex-col justify-end items-center flex '>
+              {navLinks.map((nav, index) => (
+              <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${index === navLinks.length-1 ? 'mr-0' : 'mb-4' } `} > 
+                <a href={`#${nav.id}`}>
+                {nav.title}
+                </a>
+              </li>
+              ))}
+            </ul>
+          </div>
+      </div>
+
+    </nav>
   )
 }
 
