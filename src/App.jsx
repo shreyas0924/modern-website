72da7499a2590b13e6c2a0e2359161c3c2e258ba
shreyas0924 <shreyasp0924@gmail.com>
2022-10-10 20:11:41 +0530

    Boilerplate for all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import styles from './style'
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`${styles.flexStart} bg-primary`}>
+        <div className = {` ${styles.boxWidth} `}>
+          Hero
+        </div>
+      </div>
+      
+      
+      <div className={`${styles.flexStart} bg-primary ${styles.paddingX}`}>
+        <div className = {` ${styles.boxWidth} `}>
+          Stats
+          business
+          billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA 
+          Footer
+        </div>
+      </div>
     </div>
   )
 }
